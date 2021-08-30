7bdac739c25983aaa4855dc124d087e87435dcb3
sunknife <ilya.rubinsky@gmail.com>
2021-08-30 14:24:58 +0300

    Project Sources added

---
+++ src/View.java
@@ -0,0 +1,11 @@
+public class View {
+
+    public static final String WELCOME_MESSAGE = "Hello, you should guess a number in range from " + Model.MIN_VALUE + " to " + Model.MAX_VALUE;
+    public static final String TRY = "Your try: ";
+    public static final String WRONG_INPUT = "This is not a number";
+    public static final String WIN = "You guessed the number! Congratulation! \nYour tries were:";
+
+    public void print(String message){
+        System.out.println(message);
+    }
+}
